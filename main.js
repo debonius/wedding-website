49b406158fa1b7c3035ed5f75a81842bf55a1444
francky <francky@tuta.io>
2022-12-12 00:19:27 +0100

    upload project files

---
+++ main.js
@@ -0,0 +1,10 @@
+const daysLeft = document.querySelector('.days-left');
+const currentDate = new Date();
+const weddingDate = new Date("01-08-2023");
+
+let difference = Math.floor( ( ( currentDate.getTime() - weddingDate.getTime() ) / (1000*3600*24) ) );
+difference *= -1;
+// console.log(currentDate);
+// console.log(weddingDate);
+// console.log(difference);
+daysLeft.innerHTML = difference;
