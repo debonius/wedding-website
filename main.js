bb9727918989b3da24882bfe455f181ee374921c
francky <francky@tuta.io>
2022-12-12 04:21:06 +0100

    fix js on day 1 - work in progress

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const daysLeft = document.querySelector('.days');
+const container = document.querySelector('.countdown');
+const label = document.querySelector('.countdown .label');
 const currentDate = new Date();
 const weddingDate = new Date("01-08-2023");
 
 let difference = Math.floor( ( ( currentDate.getTime() - weddingDate.getTime() ) / (1000*3600*24) ) );
 difference *= -1;
-// console.log(currentDate);
-// console.log(weddingDate);
-// console.log(difference);
 daysLeft.innerHTML = difference;
+daysLeft = 1;
+if (daysLeft == 1) {
+  container.appendChild = '<span class="label">days<br>left!</span>';
+}
