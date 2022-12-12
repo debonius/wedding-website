ff5ae0c4d69b66cea651f2bca54c0cd41a77cb8d
francky <francky@tuta.io>
2022-12-12 04:42:46 +0100

    fix js on day 1

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ const label = document.querySelector('.countdown .label');
 const currentDate = new Date();
 const weddingDate = new Date("01-08-2023");
 
+const labelDay1 = document.createElement('span');
+labelDay1.classList.add('label');
+labelDay1.innerHTML = 'day<br>left!';
+
+console.log(container);
+
 let difference = Math.floor( ( ( currentDate.getTime() - weddingDate.getTime() ) / (1000*3600*24) ) );
 difference *= -1;
 daysLeft.innerHTML = difference;
-daysLeft = 1;
-if (daysLeft == 1) {
-  container.appendChild = '<span class="label">days<br>left!</span>';
+
+if (difference == 1) {
+  label.remove();
+  container.appendChild(labelDay1);
 }
