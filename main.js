f4c5971a9a5ac9629f5387dc9f9051c01bd5b891
francky <francky@tuta.io>
2022-12-14 20:13:27 +0100

    simplify var names

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
 const daysLeft = document.querySelector('.days');
 const container = document.querySelector('.countdown');
 const label = document.querySelector('.countdown .label');
-const currentDate = new Date();
-const weddingDate = new Date('01-07-2023');
-// let wmm = weddingDate.getMonth()+1;
-// let wdd = weddingDate.getDate();
-// let wyyyy = weddingDate.getFullYear();
-
-// let mm = currentDate.getMonth()+1;
-// let dd = currentDate.getDate();
-// let yyyy = currentDate.getFullYear();
-// let today = `${mm}-${dd}-${yyyy}`;
-// let wedding = `0${wmm}-0${wdd}-${wyyyy}`;
-
-// let diff = today - wedding;
-// console.log(diff);
+const startDate = new Date();
+const endDate = new Date('01-07-2023');
 
 const labelDay1 = document.createElement('span');
 labelDay1.classList.add('label');
 labelDay1.innerHTML = 'day<br>left!';
 
-let difference = Math.round( ( ( currentDate - weddingDate ) / (1000*3600*24) ) );
-difference *= -1;
-daysLeft.innerHTML = difference;
+let diff = Math.round( ( ( startDate - endDate ) / (1000*3600*24) ) );
+diff *= -1;
+daysLeft.innerHTML = diff;
 
-if (difference == 1) {
+if (diff == 1) {
   label.remove();
   container.appendChild(labelDay1);
 }
 
-console.log(currentDate)
-console.log(weddingDate);
-console.log(difference)
+console.log(startDate)
+console.log(endDate);
+console.log(diff)
