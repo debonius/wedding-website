92d4a8fde2a8f64cb58bfe71030cc999c777b68b
francky <francky@tuta.io>
2022-12-14 20:15:17 +0100

    rearrange order

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const daysLeft = document.querySelector('.days');
 const container = document.querySelector('.countdown');
 const label = document.querySelector('.countdown .label');
-const startDate = new Date();
-const endDate = new Date('01-07-2023');
-
 const labelDay1 = document.createElement('span');
 labelDay1.classList.add('label');
 labelDay1.innerHTML = 'day<br>left!';
+
+const startDate = new Date();
+const endDate = new Date('01-07-2023');
 
 let diff = Math.round( ( ( startDate - endDate ) / (1000*3600*24) ) );
 diff *= -1;
 daysLeft.innerHTML = diff;
 
+console.log(startDate)
+console.log(endDate);
+console.log(diff)
+
+
 if (diff == 1) {
   label.remove();
   container.appendChild(labelDay1);
 }
-
-console.log(startDate)
-console.log(endDate);
-console.log(diff)
