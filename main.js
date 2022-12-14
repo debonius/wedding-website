fa113ec0700d4937f76529452b0c913743ae3fe5
francky <francky@tuta.io>
2022-12-14 21:17:44 +0100

    rename today date

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const labelDay1 = document.createElement('span');
 labelDay1.classList.add('label');
 labelDay1.innerHTML = 'day<br>left!';
 
-const startDate = new Date();
+const today = new Date();
 const endDate = new Date(2023,0,7,17,00);
 
-let diff = Math.round( ( ( startDate - endDate ) / (1000*3600*24) ) );
+let diff = Math.round( ( ( today - endDate ) / (1000*3600*24) ) );
 diff *= -1;
 daysLeft.innerHTML = diff;
 
-console.log(startDate)
+console.log(today)
 console.log(endDate);
 console.log(diff)
 
